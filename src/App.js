dafc51a17eff80dd3604b140d181f50f91ba22e8
Oksana Repchak <oksa.repchak@gmail.com>
2020-12-08 11:24:09 +0200

    add componentDid

--- src/App.js
+++ src/App.js
@@ -46,8 +46,18 @@ class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
   componentDidUpdate(prevProps, prevState) {
     if (this.state.contacts !== prevState.contacts) {
+      // Контакти записуються в сховище
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
